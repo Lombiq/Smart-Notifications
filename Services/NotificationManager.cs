e1bdd1231f9d195f628bb4e11ec95953ec311129
Somogyi Krisztián <krisztian.somogyi@lombiq.com>
2015-02-01 15:00:58 +0100

    Code styling, ADHOC-10

--- Services/NotificationManager.cs
+++ Services/NotificationManager.cs
@@ -13,30 +13,31 @@ namespace Lombiq.SmartNotifications.Services
     {
         private readonly IRepository<StickyNotificationRecord> _notificationRepository;
 
+
         public NotificationManager(IRepository<StickyNotificationRecord> notificationRepository)
         {
             _notificationRepository = notificationRepository;
         }
 
-        public IEnumerable<IStickyNotification> GetNotifications(string SessionId)
+        public IEnumerable<IStickyNotification> GetNotifications(string sessionId)
         {
-            return _notificationRepository.Table.Where(record => record.SessionId == SessionId).OrderBy(record => record.Id);
+            return _notificationRepository.Table.Where(record => record.SessionId == sessionId).OrderBy(record => record.Id);
         }
 
-        public void SaveNotification(string SessionId, string NotificationMessage, NotifyType NotificationType)
+        public void SaveNotification(string sessionId, string notificationMessage, NotifyType notificationType)
         {   
             var notification = new StickyNotificationRecord();
             _notificationRepository.Create(notification);
-            notification.SessionId = SessionId;
-            notification.NotificationMessage = NotificationMessage;
-            notification.NotificationType = NotificationType;
+            notification.SessionId = sessionId;
+            notification.NotificationMessage = notificationMessage;
+            notification.NotificationType = notificationType;
         }
 
-        public void DeleteNotification(string SessionId, int Id)
+        public void DeleteNotification(string sessionId, int id)
         {
-            var notification = _notificationRepository.Get(Id);
+            var notification = _notificationRepository.Get(id);
             // If the second condition wouldn't be present anybody would be able to delete notifications by knowing the id of someone else's notification.
-            if (notification != null && notification.SessionId == SessionId)
+            if (notification != null && notification.SessionId == sessionId)
             {
                 _notificationRepository.Delete(notification);
             }
