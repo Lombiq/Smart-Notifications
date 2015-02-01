6bd1eeb3d0700550f2b76a9674d20774f3d2357d
Somogyi Krisztián <krisztian.somogyi@lombiq.com>
2015-02-01 14:49:36 +0100

    Argument names fixed, ADHOC-10

--- Services/INotificationManager.cs
+++ Services/INotificationManager.cs
@@ -13,23 +13,23 @@ namespace Lombiq.SmartNotifications.Services
         /// <summary>
         /// This method gets the notifications for the actual user.
         /// </summary>
-        /// <param name="SessionId">The session ID actual user.</param>
+        /// <param name="sessionId">The session ID actual user.</param>
         /// <returns>Returns the notification messages for the actual user.</returns>
-        IEnumerable<IStickyNotification> GetNotifications(string SessionId);
+        IEnumerable<IStickyNotification> GetNotifications(string sessionId);
 
         /// <summary>
         /// This method saves the notifications to database.
         /// </summary>
-        /// <param name="SessionId">The session ID of the actual user.</param>
-        /// <param name="NotificationMessage">The notification message.</param>
-        /// <param name="NotificationType">The notification type.</param>
-        void SaveNotification(string SessionId, string NotificationMessage, NotifyType NotificationType);
+        /// <param name="sessionId">The session ID of the actual user.</param>
+        /// <param name="notificationMessage">The notification message.</param>
+        /// <param name="notificationType">The notification type.</param>
+        void SaveNotification(string sessionId, string notificationMessage, NotifyType notificationType);
 
         /// <summary>
         /// This method removes the selcted record from the table.
         /// </summary>
-        /// <param name="SessionId">The session ID of the user.</param>
-        /// <param name="Id">The unique id of the notification.</param>
-        void DeleteNotification(string SessionId, int Id);
+        /// <param name="sessionId">The session ID of the user.</param>
+        /// <param name="id">The unique id of the notification.</param>
+        void DeleteNotification(string sessionId, int id);
     }
 }
