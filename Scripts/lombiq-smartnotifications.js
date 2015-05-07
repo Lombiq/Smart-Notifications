bf3dedb657d4efa381ae7aed66a3b3fd9a14b5ce
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2015-05-07 19:06:55 +0200

    Removing the whole messages container when all notifications where removed

---
+++ Scripts/lombiq-smartnotifications.js
@@ -0,0 +1,11 @@
+﻿(function ($) {
+    $.extend({
+        smartNotifications: {
+            removeMessagesContainerIfNecessary: function () {
+                if ($(".zone-messages").is(":empty") || $(".zone-messages :not(:hidden)").length == 0) {
+                    $("#messages").remove();
+                }
+            }
+        }
+    });
+})(jQuery);
